26e8184ec14488a06bc5fd6015a05a73b876a3b7
Prabhat Yadav <133522303+Prabhatyadav60@users.noreply.github.com>
2024-12-18 22:20:18 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,26 +4,24 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
 
-# Load the dataset
-dataset = pd.read_csv('E:\LUV\Project_KNN\Social_Network_Ads.csv')  # Ensure the dataset is in the same directory
 
-# Splitting data into features and target variable
-X = dataset.iloc[:, :-1].values  # Assuming last column is the target
+dataset = pd.read_csv('E:\LUV\Project_KNN\Social_Network_Ads.csv')  
+
+X = dataset.iloc[:, :-1].values 
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-# Feature scaling
+
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Train the KNN model
+
 knn_classifier = KNeighborsClassifier(n_neighbors=5, p=2)
 knn_classifier.fit(X_train, y_train)
 
-# Save the trained model and scaler
+
 pickle.dump(knn_classifier, open('knn_model.pkl', 'wb'))
 pickle.dump(scaler, open('scaler.pkl', 'wb'))
 
